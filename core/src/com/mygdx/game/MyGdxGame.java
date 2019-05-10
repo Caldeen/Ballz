ef3f981e0da7c4d9ac71c2393a77e8d77f548a41
Caldeen <jaro.tren6@gmail.com>
2019-05-10 16:24:42 +0200

    test map Parse

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.Player.Ball;
 import com.mygdx.game.Player.EventHandler;
+import com.sun.org.apache.xpath.internal.operations.Or;
 
 public class MyGdxGame extends ApplicationAdapter {
 	public static int METER_TO_PIX=16;
@@ -24,16 +25,19 @@ public class MyGdxGame extends ApplicationAdapter {
 	private TiledMapRenderer tiledMapRenderer;
 	private Box2DDebugRenderer debugRenderer;
 	private OrthographicCamera cam;
+	private OrthographicCamera viewCam;
 
 	Ball ball;
 
 	@Override
 	public void create () {
+		viewCam=new OrthographicCamera();
+		viewCam.setToOrtho(false,100,60);
 		cam=new OrthographicCamera();
 		debugRenderer=new Box2DDebugRenderer();
 		world=new World(new Vector2(0,-10),true);
 		cam.setToOrtho(false,200,120);
-
+		Gdx.graphics.setWindowedMode(1600,900);
 		batch = new SpriteBatch();
 		tiledMap = new TmxMapLoader().load("core/assets/testMapa.tmx");
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
@@ -48,8 +52,9 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		tiledMapRenderer.setView(cam);
 		cam.update();
-		batch.setProjectionMatrix(cam.combined);
-		tiledMapRenderer.setView(cam);
+		cam.position.set(ball.getBody().getWorldCenter().x,ball.getBody().getWorldCenter().y,0);
+		viewCam.update();
+		batch.setProjectionMatrix(viewCam.combined);
 
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
