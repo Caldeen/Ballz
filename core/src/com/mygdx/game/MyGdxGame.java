7dce9004e154392f58abe1d65977a286ab8f45c5
Caldeen <jaro.tren6@gmail.com>
2019-05-10 15:46:27 +0200

    test map Parse

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -11,19 +11,22 @@ import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class MyGdxGame extends ApplicationAdapter {
 	public static int METER_TO_PIX=16;
-	World world;
-	SpriteBatch batch;
-	TiledMap tiledMap;
-	TiledMapRenderer tiledMapRenderer;
-	OrthographicCamera cam;
+	private World world;
+	private SpriteBatch batch;
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+	private Box2DDebugRenderer debugRenderer;
+	private OrthographicCamera cam;
 	@Override
 	public void create () {
 		cam=new OrthographicCamera();
-		world=new World(new Vector2(0,0),true);
+		debugRenderer=new Box2DDebugRenderer();
+		world=new World(new Vector2(0,-10),true);
 		cam.setToOrtho(false,1200,1200);
 		batch = new SpriteBatch();
 		tiledMap = new TmxMapLoader().load("core/assets/testMapa.tmx");
@@ -36,12 +39,14 @@ public class MyGdxGame extends ApplicationAdapter {
 		tiledMapRenderer.setView(cam);
 		cam.update();
 		batch.setProjectionMatrix(cam.combined);
+		tiledMapRenderer.setView(cam);
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
+		debugRenderer.render(world,cam.combined);
 		tiledMapRenderer.render();
+
 		batch.begin();
 
 		batch.end();
